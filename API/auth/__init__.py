474220b664f2754bfa171648b90fbb414e592b0d
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-03-15 14:13:57 +0100

    added parameter description

--- API/auth/__init__.py
+++ API/auth/__init__.py
@@ -43,7 +43,7 @@ def get_osm_auth_user(access_token):
     return user
 
 
-def login_required(access_token: str = Header(...)):
+def login_required(access_token: str = Header(..., description="Access token to authorize user")):
     return get_osm_auth_user(access_token)
 
 
