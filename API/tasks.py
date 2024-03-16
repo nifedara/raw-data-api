6516ea72cba3a108ffc67397b323fdfc8112cf39
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-03-16 10:23:13 +0100

    updated 500 response

--- API/tasks.py
+++ API/tasks.py
@@ -16,8 +16,7 @@ from .auth import AuthUser, admin_required, login_required, staff_required
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 
-@router.get("/status/{task_id}", response_model=SnapshotTaskResponse,
-            responses={'500': {"model": ErrorMessage}})
+@router.get("/status/{task_id}", response_model=SnapshotTaskResponse, responses={'500': {}})
 @version(1)
 def get_task_status(
     task_id,
