045774bd4f64f30c383d675fc9e09dcf3ee04e00
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2024-04-04 11:03:19 +0100

    added response, response model, examples

--- API/hdx.py
+++ API/hdx.py
@@ -9,13 +9,20 @@ from src.config import RATE_LIMIT_PER_MIN
 
 from .auth import AuthUser, admin_required, staff_required
 
-# from src.validation.models import DynamicCategoriesModel
+from src.validation.models import ErrorMessage, common_responses
 
 
 router = APIRouter(prefix="/hdx", tags=["HDX"])
 
 
-@router.post("", response_model=dict)
+@router.post(
+    "",
+    response_model=dict,
+    responses={
+        "200": {"content": {"application/json": {"example": {"create": True}}}},
+        **common_responses,
+    },
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def create_hdx(
@@ -36,7 +43,11 @@ async def create_hdx(
     return hdx_instance.create_hdx(hdx_data)
 
 
-@router.get("", response_model=List[dict])
+@router.get(
+    "",
+    response_model=List[dict],
+    responses={"422": {"model": ErrorMessage}, "500": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def read_hdx_list(
@@ -66,11 +77,15 @@ async def read_hdx_list(
     try:
         hdx_list = hdx_instance.get_hdx_list_with_filters(skip, limit, filters)
     except Exception as ex:
-        raise HTTPException(status_code=422, detail="Couldn't process query")
+        raise HTTPException(status_code=422, detail=[{"msg": "Couldn't process query"}])
     return hdx_list
 
 
-@router.get("/search", response_model=List[dict])
+@router.get(
+    "/search",
+    response_model=List[dict],
+    responses={"404": {"model": ErrorMessage}, "500": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def search_hdx(
@@ -98,7 +113,11 @@ async def search_hdx(
     return hdx_list
 
 
-@router.get("/{hdx_id}", response_model=dict)
+@router.get(
+    "/{hdx_id}",
+    response_model=dict,
+    responses={"404": {"model": ErrorMessage}, "500": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def read_hdx(
@@ -115,16 +134,20 @@ async def read_hdx(
         dict: Details of the requested HDX entry.
 
     Raises:
-        HTTPException: If the HDX entry is not found.
+        HTTPException 404: If the HDX entry is not found.
     """
     hdx_instance = HDX()
     hdx = hdx_instance.get_hdx_by_id(hdx_id)
     if hdx:
         return hdx
-    raise HTTPException(status_code=404, detail="HDX not found")
+    raise HTTPException(status_code=404, detail=[{"msg": "HDX not found"}])
 
 
-@router.put("/{hdx_id}", response_model=dict)
+@router.put(
+    "/{hdx_id}",
+    response_model=dict,
+    responses={**common_responses, "404": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def update_hdx(
@@ -146,17 +169,21 @@ async def update_hdx(
         dict: Result of the HDX update process.
 
     Raises:
-        HTTPException: If the HDX entry is not found.
+        HTTPException 404: If the HDX entry is not found.
     """
     hdx_instance = HDX()
     existing_hdx = hdx_instance.get_hdx_by_id(hdx_id)
     if not existing_hdx:
-        raise HTTPException(status_code=404, detail="HDX not found")
+        raise HTTPException(status_code=404, detail=[{"msg": "HDX not found"}])
     hdx_instance_update = HDX()
     return hdx_instance_update.update_hdx(hdx_id, hdx_data)
 
 
-@router.patch("/{hdx_id}", response_model=Dict)
+@router.patch(
+    "/{hdx_id}",
+    response_model=Dict,
+    responses={**common_responses, "404": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def patch_hdx(
@@ -178,17 +205,21 @@ async def patch_hdx(
         Dict: Result of the HDX update process.
 
     Raises:
-        HTTPException: If the HDX entry is not found.
+        HTTPException 404: If the HDX entry is not found.
     """
     hdx_instance = HDX()
     existing_hdx = hdx_instance.get_hdx_by_id(hdx_id)
     if not existing_hdx:
-        raise HTTPException(status_code=404, detail="HDX not found")
+        raise HTTPException(status_code=404, detail=[{"msg": "HDX not found"}])
     patch_instance = HDX()
     return patch_instance.patch_hdx(hdx_id, hdx_data)
 
 
-@router.delete("/{hdx_id}", response_model=dict)
+@router.delete(
+    "/{hdx_id}",
+    response_model=dict,
+    responses={**common_responses, "404": {"model": ErrorMessage}},
+)
 @limiter.limit(f"{RATE_LIMIT_PER_MIN}/minute")
 @version(1)
 async def delete_hdx(
@@ -208,11 +239,11 @@ async def delete_hdx(
         dict: Result of the HDX deletion process.
 
     Raises:
-        HTTPException: If the HDX entry is not found.
+        HTTPException 404: If the HDX entry is not found.
     """
     hdx_instance = HDX()
     existing_hdx = hdx_instance.get_hdx_by_id(hdx_id)
     if not existing_hdx:
-        raise HTTPException(status_code=404, detail="HDX not found")
+        raise HTTPException(status_code=404, detail=[{"msg": "HDX not found"}])
 
     return hdx_instance.delete_hdx(hdx_id)
